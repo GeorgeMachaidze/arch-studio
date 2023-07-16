3e254b93ba1300458c9ea1d35bab8dce1fbc35ce
Wakana <wakanichi13@gmail.com>
2023-07-16 19:37:04 +0400

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import styled from "styled-components";
+import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
-    <>
-      <div></div>
-    </>
+    <Main>
+      <Routes>
+        <Route path="" element={"home"} />
+      </Routes>
+    </Main>
   );
 }
 
 export default App;
+
+const Main = styled.main`
+  background-color: #fff;
+  width: 100%;
+  height: 100%n;
+`;
